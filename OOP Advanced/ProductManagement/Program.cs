861cf6135c9e04d0ae28daa05d18339251c11a65
DangDucThuan38 <dangducthuan999@gmail.com>
2022-11-23 21:47:32 +0700

    change

---
+++ OOP Advanced/ProductManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductManagement // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+           Shop s1= new Shop();
+           s1.Run();
+
+        }
+    }
+}
