ed2f9717b4a94791378a30290f646c71ed2bf000
DangDucThuan38 <dangducthuan999@gmail.com>
2022-12-01 20:42:45 +0700

    demo exception

--- OOP Advanced/DemoException/Program.cs
+++ OOP Advanced/DemoException/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//try
+//{
+//    System.Console.Write("Enter A:");
+//    int a=int.Parse(Console.ReadLine());
+//    System.Console.Write("Enter B:");
+//    int b=int.Parse(Console.ReadLine());
+//    int c= a/b;
+//    System.Console.WriteLine("Result: "+c);
+//}
+//catch (FormatException)
+//{
+//    System.Console.WriteLine("Invalid Input");
+//}
+//catch(DivideByZeroException)
+//{
+//    System.Console.WriteLine("Divide by zero");
+//}
+
+
+
+try
+{
+    System.Console.WriteLine("Enter length of array:");
+    int n = int.Parse(Console.ReadLine());
+    int[] a = new int[n];
+    int sum = 0; //Chưa khai báo kiểu
+    for (int i = 0; i < n; i++) // Erro IndexOutOfRangException.
+    {
+        System.Console.WriteLine("Enter a[{0}]: ", i);
+        a[i] = int.Parse(Console.ReadLine());
+        sum += a[i];
+    }
+    System.Console.WriteLine("Enter lower: ");
+    int lower = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("Enter upper: ");
+    int upper = int.Parse(Console.ReadLine());
+    int partialSum = 0;
+    for (int i = lower; i <= upper; i++)// <=upper để lấy số
+    {
+        partialSum += a[i];
+
+    }
+    double rate = partialSum * 1.0 / sum;
+    System.Console.WriteLine("Rate: " + rate);
+}
+catch (FormatException)
+{
+    System.Console.WriteLine("Input Invaild");
+}catch(DivideByZeroException)
+{
+    System.Console.WriteLine("Divide By Zero");
+}catch(IndexOutOfRangeException)
+{
+    System.Console.WriteLine("Index Out Range");
+}
---
+++ OOP Advanced/DemoException02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int a = ReadInt("Enter a: ");
+        int b = ReadInt("Enter b: ");
+        double e=ReadDouble("Enter e: ");
+        int c=a/b;
+        System.Console.WriteLine("Result: "+c);
+    }
+    public static int  ReadInt(string prompt)
+    {
+        bool invalid = true;
+        int n = 0;
+        while (invalid)
+        {
+            try
+            {
+                Console.Write(prompt);
+                n = int.Parse(Console.ReadLine());
+                invalid = false;
+            }
+            catch (FormatException)
+            {
+
+                System.Console.WriteLine("Input Valid .Please try Again.!");
+            }
+
+        }
+        return n;
+    }
+
+   public static double ReadDouble(string get)
+   {
+         bool invalid = true;
+        double n = 0;
+        while (invalid)
+        {
+            try
+            {
+                Console.Write(get);
+                n = double.Parse(Console.ReadLine());
+                invalid = false;
+            }
+            catch (FormatException)
+            {
+
+                System.Console.WriteLine("Input Valid .Please try Again.!");
+            }
+
+        }
+        return n;
+
+   }
+}
+
